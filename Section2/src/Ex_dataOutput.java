17726a6b409fe8ac92fe3cf297ebe2b27ac5afdd
VitorDLeo <vitordileo003@gmail.com>
2022-05-28 16:45:16 -0300

    data output

---
+++ Section2/src/Ex_dataOutput.java
@@ -0,0 +1,28 @@
+import java.util.Locale;
+
+public class Ex_dataOutput {
+
+    public static void main(String[] args) {
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+
+        int age = 30;
+        int code = 5290;
+        char gender = 'F';
+
+        double price1 = 2100.0;
+        double price2 = 650.50;
+        double measure = 53.234567;
+
+        System.out.println("Products:");
+        System.out.println(product1 + " , which porice is $ " + price1);
+        System.out.println(product2 + " , which price is $ " + price2);
+        System.out.println(" ");
+        System.out.printf("Record: %d years old, code %d and gender: %s %n%n", age, code, gender);
+        System.out.println("Measue eight decimal places: " + measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f", measure);
+    }
+}

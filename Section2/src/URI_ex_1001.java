4c1f07d76447ac3e53718d0eaecd8212a8b060fd
VitorDLeo <vitordileo003@gmail.com>
2022-05-28 21:35:56 -0300

    Ex URI 1001

---
+++ Section2/src/URI_ex_1001.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class URI_ex_1001 {
+    public static void main(String[] args) {
+        
+        int A, B, X;
+
+        Scanner sc = new Scanner(System.in);
+
+        A = sc.nextInt();
+        B = sc.nextInt();
+
+        X = A + B;
+
+        System.out.println("X = " + X);
+    }
+}

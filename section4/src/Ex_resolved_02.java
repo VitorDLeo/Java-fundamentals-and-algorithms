9081e389c6b95d863b4629e186dbd42bb7ae96c9
VitorDLeo <vitordileo003@gmail.com>
2022-05-31 18:32:17 -0300

    URI Ex 1113

---
+++ section4/src/Ex_resolved_02.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Ex_resolved_02 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int age, mediaAge, sumAge, count;
+
+        mediaAge = 0;
+        count = 0;
+        sumAge = 0;
+        age = sc.nextInt();
+
+        while (age >= 0){
+            if (age >= 0){
+                sumAge += age;
+                count += 1;
+            } else if (age < 0) {
+                System.out.println("Impossivel de calcular");
+            }
+            age = sc.nextInt();
+        }
+        
+        mediaAge = sumAge / count;
+        System.out.println("Sum Age: " + mediaAge);
+    }   
+}

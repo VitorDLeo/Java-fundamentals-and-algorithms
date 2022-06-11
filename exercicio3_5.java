7bade98dc85c72da0f24849a9f2974b666fef936
VitorDLeo <vitordileo003@gmail.com>
2022-06-11 01:58:59 -0300

    exercicio 3.6

--- exercicio3_5.java
+++ exercicio3_5.java
@@ -1,2 +1,17 @@
 public class exercicio3_5 {
+    public static void main(String[] args) {
+        int x = 13;
+        while (x != 1){
+            if (x % 2 == 0){
+                x += x/2;
+            } else if (x % 2 == 1){
+                x += 3 * x + 1;
+            }
+            if (x == 1){
+                break;
+            }
+            System.out.print(x + "->");
+            x++;
+        }
+    }
 }

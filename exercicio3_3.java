ceae08db1d53b0843a35f4e0ed93a279f5bde919
VitorDLeo <vitordileo003@gmail.com>
2022-06-11 01:54:27 -0300

    exercicio 3.5

--- exercicio3_3.java
+++ exercicio3_3.java
@@ -1,5 +1,10 @@
 public class exercicio3_3 {
     public static void main(String[] args) {
-        
+       for(int i = 1; i <= 100; i++){
+           if (i % 3 == 0){
+               System.out.println(i);
+           }
+       }
+    System.out.println("FIM");
     }
 }
